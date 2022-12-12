7a9c93406208ed9df394eda8f0bf691cc2ed343e
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-12-12 12:04:51 +0000

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -1,3 +1,3 @@
-uncompress /app/netphos-3.1.Linux.tar.Z && \
-tar -xvf /app/netphos-3.1.Linux.tar && \
+uncompress /app/netphos-archive/netphos-3.1.Linux.tar.Z && \
+tar -xvf /app/netphos-archive/netphos-3.1.Linux.tar && \
 yes | cp -rf /app/ape.docker /app/ape-1.0/ape
